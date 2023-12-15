d8e2e3c7b22a57bc7d060d7abbba9541bc2e0c68
Akamo Dolapo <dolapoakamo01@gmail.com>
2023-12-15 20:21:03 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+// function sendEmail() {
+//     const form = document.getElementById('myForm');
+//     emailjs.sendForm('service_jskyqrf', 'template_ric9fiq', form, 'NX4ay_14xkpKvce2S')
+//       .then(function(response) {
+//         console.log('SUCCESS!', response.status, response.text);
+//         // Optionally, add success message or redirect to a thank-you page
+//         location.reload(); // This line reloads the page after successful submission
+//       }, function(error) {
+//         console.log('FAILED...', error);
+//         // Optionally, show an error message to the user
+//       });
+//   }
